ec8d7258ca23055d87df2c60f70d0d8b69aa473f
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-01-05 23:04:10 +0600

    New pages and test added

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 
-from .views import homePageView
+from .views import HomePageView, AboutPageView
 
 
 urlpatterns = [
-
-    path('', homePageView, name='home')
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
